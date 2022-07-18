9388328d2f3d80827cd0138df74f0b23818aaf50
stepvizzy <stepvizzy@users.noreply.github.com>
2022-07-18 09:06:56 +0000

    automated commit by submit50

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from cs50 import SQL
 from datetime import datetime
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import login_required, check
